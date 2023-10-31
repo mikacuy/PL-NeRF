d85c49103f085d2ec8bc8134ae6268cd1bca071e
mikacuy <muy@adobe.com>
2023-10-30 23:35:08 -0700

    initial commit

--- depth_supervised_exps/model/__init__.py
+++ depth_supervised_exps/model/__init__.py
@@ -3,4 +3,3 @@ from .run_nerf_helpers import NeRF, get_embedder, get_rays, sample_pdf, img2mse,
     sample_pdf_reformulation, sample_pdf_reformulation_joint, \
     get_space_carving_idx, \
     sample_pdf_return_u, sample_pdf_joint_return_u, sample_pdf_reformulation_return_u, sample_pdf_reformulation_joint_return_u
-from .cspn import resnet18_skip
